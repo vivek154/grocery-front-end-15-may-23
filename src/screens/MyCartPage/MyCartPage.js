439c9470fa6bd20074bf12a82dba9df416681ab4
vivek154 <vivekkhandekar154@gmail.com>
2023-05-17 18:22:02 +0530

    changes urls in api.js as per backend changes,added Act Ind. in Delivery.js,added mycart reload flag to redux store

--- src/screens/MyCartPage/MyCartPage.js
+++ src/screens/MyCartPage/MyCartPage.js
@@ -1,5 +1,10 @@
-
-import {ActivityIndicator, ScrollView, StyleSheet, Text, View} from 'react-native';
+import {
+  ActivityIndicator,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import React from 'react';
 import PageHeader from '../PageHeader/PageHeader';
 import MyCartCard from './MycartCard/MyCartCard';
@@ -10,58 +15,57 @@ import DeleteIcon from '../../svg/delete.svg';
 import {getmycart} from '../../api/api';
 import {useDispatch, useSelector} from 'react-redux';
 import {Store} from '../../redux/Store';
-import OrderDetails from './OrderDetails';
 
 const MyCartPage = ({navigation}) => {
-  
   function showDeliveryOptions() {
-    dispatch({type:AUTH_TYPE.SET_MY_ORDERS,payload:data})
-    console.log("*****My Orders after buy now*****",Store.getState().auth.myOrders)
+    dispatch({type: AUTH_TYPE.SET_MY_ORDERS, payload: data});
     navigation.navigate('Delivery');
   }
   const [data, setdata] = useState([]);
   const [totalPrice, setTotalPrice] = useState(0);
-  const [fetchingDataIndicator,setFetchingDataIndicator]=useState(true)
+  const [fetchingDataIndicator, setFetchingDataIndicator] = useState(true);
   const dispatch = useDispatch();
   const {userData} = useSelector(state => state?.auth);
+  const reloadMyCartScreen = useSelector(
+    state => state.auth.reloadMyCartScreen,
+  );
 
+  console.log("*****reload my cart screen*******",reloadMyCartScreen)
 
-  
-  function getTotalPrice(){
-    let state1=Store.getState()
-    console.log("*****totalPrice***",state1.auth.totalPrice)
+  function getTotalPrice() {
+    let state1 = Store.getState();
   }
   const makeApiRequest = async () => {
     try {
       let response = await getmycart(userData.id);
       console.log('get my cart response ', response.data);
-      let payload = response.data//.map(cart => cart.productId);
+      let payload = response.data; //.map(cart => cart.productId);
       dispatch({type: AUTH_TYPE.GET_MY_CART_DATA, payload: payload});
       setdata(response.data);
-      setFetchingDataIndicator(false)
+      setFetchingDataIndicator(false);
     } catch (err) {
       console.log(err);
     }
   };
   useEffect(() => {
     makeApiRequest();
-  }, []);
-  
-  useEffect(()=>{
-    dispatch({type:AUTH_TYPE.SET_TOTAL_PRICE,payload:totalPrice})
-    getTotalPrice()
-  },[totalPrice])
-  return (
+  }, [reloadMyCartScreen]);
 
-    <View style={{flex: 1, backgroundColor: 'white'}}>      
-          <View style={{height: '13%'}}>
-            <PageHeader text="My Cart" navigation={navigation} navigateTo={"ProductList"}></PageHeader>
-          </View>
-        {
-          fetchingDataIndicator && <ActivityIndicator></ActivityIndicator>
-        }
-        {!fetchingDataIndicator && (data.length>0)&& ( 
-          <> 
+  useEffect(() => {
+    dispatch({type: AUTH_TYPE.SET_TOTAL_PRICE, payload: totalPrice});
+    getTotalPrice();
+  }, [totalPrice]);
+  return (
+    <View style={{flex: 1, backgroundColor: 'white'}}>
+      <View style={{height: '13%'}}>
+        <PageHeader
+          text="My Cart"
+          navigation={navigation}
+          navigateTo={'ProductList'}></PageHeader>
+      </View>
+      {fetchingDataIndicator && <ActivityIndicator></ActivityIndicator>}
+      {!fetchingDataIndicator && data.length > 0 && (
+        <>
           <View style={{height: '77%'}}>
             <ScrollView>
               <View>
@@ -88,25 +92,25 @@ const MyCartPage = ({navigation}) => {
               </Text>
             </View>
             <View>
-              {(data.length > 0) && 
-              <Mybutton
-                btnTxt="Buy Now "
-                txtColor="white"
-                myButton={styles.myButton}
-                onPress={showDeliveryOptions}
-                data={data}></Mybutton>
-              }  
+              {data.length > 0 && (
+                <Mybutton
+                  btnTxt="Buy Now "
+                  txtColor="white"
+                  myButton={styles.myButton}
+                  onPress={showDeliveryOptions}
+                  data={data}></Mybutton>
+              )}
             </View>
           </View>
-          
         </>
       )}
-      {
-        !fetchingDataIndicator && (data.length==0) &&
-        <View style={{flex:1,justifyContent:"center",alignItems:"center"}}>
-          <Text style={{color:"black",fontSize:17,fontWeight:"bold"}}>Your Cart is Empty</Text>
+      {!fetchingDataIndicator && data.length == 0 && (
+        <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+          <Text style={{color: 'black', fontSize: 17, fontWeight: 'bold'}}>
+            Your Cart is Empty
+          </Text>
         </View>
-      }
+      )}
     </View>
   );
 };
